dfcedd9938002ce9138dc4dd4f3691a66d5fe589
Brett Fuller <bfuller123@gmail.com>
2017-05-06 00:26:19 -0500

    added funcs randomword, stringtoarray, arraytoblanks

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -1,4 +1,27 @@
 var Hangman = {
-  userGuess: document.keyup;
-  answer: choose random word from list of words
-}
+  userGuess: document.keyup,
+  answer: [],
+  lettersGuessed: [],
+  guessesLeft: 6,
+  board: [],
+};
+
+function getRandomWord(list) {
+  var randomNum = Math.floor(Math.random() * list.length);
+  return list[randomNum];
+};
+
+function stringToArray(str) {
+  return str.split('');
+};
+
+function arrayToBlanks(arr) {
+  arr.forEach(function(element){
+    if(element == ' ' || element == ',' || element == "'") {
+      Hangman.board.push(element);
+    }
+    else {
+      Hangman.board.push('_');
+    }
+  });
+};
